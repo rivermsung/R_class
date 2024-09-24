e32699e929e4e09e05bca05ccb42511b99654bff
Daniel Hong <dan910803@gmail.com>
2024-09-24 16:15:43 +0800

    remember capital

---
+++ manipulation.R
@@ -0,0 +1,51 @@
+#summarize data
+summary()
+
+#
+class()
+
+#examine the structure of the object
+str()
+
+#$ aka column
+
+#table can be from internet
+
+#object[no.row,no.column]
+
+#set up condition(== to be) to see which match the condition
+students$gender=="female"
+
+#create filter
+filter<-students$gender=="female"
+
+#make a filter into object
+students[filter,]->female
+
+#change row or col names(c for combine)
+rownames(females)<-c('Vanessa', 'Vicky', 'Michelle', 'Joyce', 'Victoria')
+
+#:means every number between
+1:nrow(female)
+
+#sample 2 random object in 'female' in the filter 'sf'
+sf<-sample(1:nrow(female), 2)
+
+#create a vector with order
+ind1<-order (students$height)
+
+#sorting data set with  with filter
+students [ind1,]
+
+#shorten into
+students[order(students$height),]
+
+# If `condition` then give 'blue'. If not then gives 'red'.
+colors<-ifelse(students$gender=='male', 'blue','red') 
+
+# create a new column
+students$colors<-ifelse(students$gender=='male','blue','red') 
+
+#delete column
+data$column<-NULL
+
